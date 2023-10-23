1693ed101f0583d5cb1f2df09eb69620f86a51f2
shrutisindagi <148736444+shrutisindagi@users.noreply.github.com>
2023-10-23 00:09:31 -0400

    Update streamlit_app.py
    Removed the streamlit.dataframe and used streamlit.multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,4 @@ streamlit.text('🐔 Hard-Boiled Free-Range Eggs')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index),['Avocado', 'Strawberries'])
